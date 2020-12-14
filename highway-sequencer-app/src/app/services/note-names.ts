b76237af525efc57bc24d86ce302e15d56660f52
Dariush <dariush007@gmx.ch>
2020-12-14 01:54:48 +0100

    gh-pages deployment

---
+++ highway-sequencer-app/src/app/services/note-names.ts
@@ -0,0 +1,1 @@
+export const noteNames =   ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
--- highway-sequencer-app/src/app/services/sound-object.service.ts
+++ highway-sequencer-app/src/app/services/sound-object.service.ts
@@ -1,5 +1,5 @@
-import { SoundObject } from './../models/sound-object.model';
-import { soundObjectList } from './sound-object-list';
+import { SoundObject } from 'src/app/models/sound-object.model';
+import { noteNames } from './note-names';
 
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -17,15 +17,43 @@ export class SoundObjectService {
   public get soundObjects(): SoundObject[] {
     return this._soundObjects.value;
   }
-  public set setSoundObjectPosition(newPosObj: { name: string, position: { left: number, top: number, width: number, height: number} }) {
+  public set setSoundObjectPosition(newPosObj: { name: string, position: { left: number, top: number, width: number, height: number } }) {
+
+    this._soundObjects.next( this.getNewStateWithValues(newPosObj.name, "position", newPosObj.position))
+  }
+  public set setTriggered(newTriggeredObjName: string) {
+    this._soundObjects.next(this.getNewStateWithValues(newTriggeredObjName,"triggered",true))
+  }
+  private getNewStateWithValues(name: string, propertyName: string, propertyValue: any):SoundObject[] {
     let newSoundObjects = this.soundObjects
-    newSoundObjects[newSoundObjects.indexOf(newSoundObjects.find(soundObject => soundObject.name == newPosObj.name))].position = newPosObj.position
-    this._soundObjects.next(newSoundObjects)
+    newSoundObjects[newSoundObjects.indexOf(newSoundObjects.find(soundObject => soundObject.name == name))][propertyName] = propertyValue
+    return newSoundObjects
+  }
+  public set setNotTriggered(newTriggeredObjName: string) {
+    this._soundObjects.next(this.getNewStateWithValues(newTriggeredObjName,"triggered",false))
   }
   constructor() {
+    //let soundObjects: SoundObject[] = []
+    let aOctave = []
+    const numberOfOctaves = 4
+    for (let i = 0; i < numberOfOctaves; i++) {
+      let incomingNotes = noteNames.map(note => {
+      return {
+        name: note+(i + 2),
+        triggered: false,
+        position: {
+          left: 0,
+          top: 0,
+          width: 0,
+          height: 0
+        }
+      }
+    })
+  
+      aOctave.push(...incomingNotes)
+    }
 
-    // init sound objects
-    this._soundObjects.next(soundObjectList);
+    this._soundObjects.next(aOctave);
 
   }
 }
