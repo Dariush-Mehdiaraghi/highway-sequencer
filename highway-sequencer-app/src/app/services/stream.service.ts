5af58fae6a3bc13e199d813f356f3a5ef1fc7767
remarkies <luka@ichbinich.ch>
2020-12-15 17:57:22 +0100

    Update stream.service.ts

--- highway-sequencer-app/src/app/services/stream.service.ts
+++ highway-sequencer-app/src/app/services/stream.service.ts
@@ -36,7 +36,6 @@ export class StreamService {
     //streamsFound.push({ id: 2, url: "https://s3.ipcamlive.com/streams/035fac90be7f0b35c/stream.m3u8", videoType: "application/x-mpegURL", name: "england"});
     streamsFound.push({ id: 3, url: "https://wzmedia.dot.ca.gov/D3/5_pocket.stream/chunklist_w22356296.m3u8", videoType: "application/x-mpegURL", name: "sacramento"});
     //streamsFound.push({ id: 3, url: "https://www.youtube.com/embed/XDzc5u0zvmU", videoType: "iframe", name: "Shin-Midōsuji"});
-    streamsFound.push({ id: 4, url: "https://wzmedia.dot.ca.gov/D3/99_lincoln.stream/chunklist_w691555454.m3u8", videoType: "application/x-mpegURL", name: "yuba city"});
     //https://www.earthcam.com/usa/louisiana/neworleans/bourbonstreet/?cam=catsmeow2
     streamsFound.push({ id: 5, url: "https://stream.talgov.net/Cameras/City_CCM21.stream/chunklist_w807356817.m3u8", videoType: "application/vnd.apple.mpegurl", name: "Tennessee"});
     //https://www.abbeyroad.com/Crossing
@@ -45,10 +44,15 @@ export class StreamService {
     streamsFound.push({ id: 7, url: "https://strmr5.sha.maryland.gov/rtplive/9d00119e00770051004de33696235daa/chunklist_w1558685567.m3u8", videoType: "application/vnd.apple.mpegurl", name: "Earthcam TV"});
 
     streamsFound.push({ id: 8, url: "https://5d68870966db6.streamlock.net/EzeAppCameras/KTCamera.stream/chunklist_w117724110.m3u8", videoType: "application/vnd.apple.mpegurl", name: "Kambodscha"});
-    https://www.earthcam.com/world/ireland/dublin/?cam=templebar
-    streamsFound.push({ id: 9, url: "https://videos-3.earthcam.com/fecnetwork/4054.flv/chunklist_w996964948.m3u8",videoType: "application/vnd.apple.mpegurl", name: "dublin"});
 
-    
+
+    streamsFound.push({id: 10, url: "https://wzmedia.dot.ca.gov/D3/80_crystal_springs.stream/chunklist_w400128743.m3u8",videoType: "application/x-mpegURL", name: "Hwy 80 at Crystal Springs"})
+    streamsFound.push({id: 11, url: "https://wzmedia.dot.ca.gov/D3/5_seamas.stream/chunklist_w1800298206.m3u8",videoType: "application/x-mpegURL", name: "Hwy 5 at Seamas"})
+    streamsFound.push({id: 12, url: "https://wzmedia.dot.ca.gov/D4/E12_EOF_Napa_County_Line.stream/chunklist_w616192674.m3u8",videoType: "application/x-mpegURL", name: "Fairfield"})
+    streamsFound.push({id: 13, url: "https://wzmedia.dot.ca.gov/D4/W4_JWO_Railroad_Av.stream/chunklist_w2086500761.m3u8",videoType: "application/x-mpegURL", name: "Pittsburg"})
+    streamsFound.push({id: 14, url: "https://wzmedia.dot.ca.gov/D4/W4_at_Contra_Loma_Bl_L_St.stream/chunklist_w1598218326.m3u8",videoType: "application/x-mpegURL", name: "Antioch"})
+
+
     return streamsFound;
   }
 }
